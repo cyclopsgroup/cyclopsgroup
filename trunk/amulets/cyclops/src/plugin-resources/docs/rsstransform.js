49aca352e91179b33ed07205e02ea1f45290d29d
Jiaqi Guo <jiaqi.guo@gmail.com>
2004-04-21 03:16:05 +0000

    cyclops xdoc goal changed for maven new version

--- trunk/amulets/cyclops/src/plugin-resources/docs/rsstransform.js
+++ trunk/amulets/cyclops/src/plugin-resources/docs/rsstransform.js
@@ -1,11 +1,15 @@
 function rsstransform(rssHref, stylesheetHref) {
-    var rssDocument = new ActiveXObject("MSXML2.DOMDocument");
-    rssDocument.async=false;
-    rssDocument.load(rssHref);
-    
-    var rssStylesheet = new ActiveXObject("MSXML2.DOMDocument");
-    rssStylesheet.async=false;
-    rssStylesheet.load(stylesheetHref);
-    
-    return rssDocument.transformNode(rssStylesheet);
+    try{
+        var rssDocument = new ActiveXObject("MSXML2.DOMDocument");
+        rssDocument.async=false;
+        rssDocument.load(rssHref);
+        
+        var rssStylesheet = new ActiveXObject("MSXML2.DOMDocument");
+        rssStylesheet.async=false;
+        rssStylesheet.load(stylesheetHref);
+        
+        return rssDocument.transformNode(rssStylesheet);
+    } catch (e) {
+        document.write("<p>" + e + "</p>");
+    }
 }
