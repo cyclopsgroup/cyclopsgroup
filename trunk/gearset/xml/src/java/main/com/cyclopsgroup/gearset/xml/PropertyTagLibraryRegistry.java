b50e3be0cf7b5ed012c69bf217afed378bb7225e
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2004-08-20 04:41:36 +0000

    *** empty log message ***

--- trunk/gearset/xml/src/java/main/com/cyclopsgroup/gearset/xml/PropertyTagLibraryRegistry.java
+++ trunk/gearset/xml/src/java/main/com/cyclopsgroup/gearset/xml/PropertyTagLibraryRegistry.java
@@ -25,46 +25,67 @@ import org.apache.commons.jelly.JellyContext;
 import org.apache.commons.jelly.TagLibrary;
 import org.apache.commons.lang.StringUtils;
 
+import com.cyclopsgroup.gearset.beans.SimpleLogEnabled;
+
 /**
  * Base tag library registry class which takes a given properties file as registry
  * 
  * @author <a href="mailto:jiiaqi@yahoo.com">Jiaqi Guo </a>
  */
-public abstract class PropertyTagLibraryRegistry
+public abstract class PropertyTagLibraryRegistry extends SimpleLogEnabled
 {
+
+    /** Empty url array */
+    public static final URL[] EMPTY_URL_ARRAY = new URL[0];
+
     private HashMap tagLibraries = new HashMap();
 
     /**
      * Constructor of PropertyTagLibraryRegistry
-     * 
-     * @throws Exception
      */
-    public PropertyTagLibraryRegistry() throws Exception
+    public PropertyTagLibraryRegistry()
     {
-        URL[] resources = getRegistryResources();
-        for (int i = 0; i < resources.length; i++)
+        try
         {
-            URL resource = resources[i];
-            Properties props = new Properties();
-            props.load(resource.openStream());
-            for (Iterator j = props.keySet().iterator(); j.hasNext();)
+            URL[] resources = getRegistryResources();
+            for (int i = 0; i < resources.length; i++)
             {
-                String key = (String) j.next();
-                if (key.endsWith(".url"))
+                URL resource = resources[i];
+                Properties props = new Properties();
+                try
                 {
-                    String url = props.getProperty(key);
-                    String name = StringUtils.chomp(key, ".url");
-                    String libraryName = props.getProperty(name + ".library");
-                    if (StringUtils.isEmpty(libraryName))
+                    props.load(resource.openStream());
+                    for (Iterator j = props.keySet().iterator(); j.hasNext();)
                     {
-                        continue;
+                        String key = (String) j.next();
+                        if (key.endsWith(".url"))
+                        {
+                            String url = props.getProperty(key);
+                            String name = StringUtils.chomp(key, ".url");
+                            String libraryName = props.getProperty(name
+                                    + ".library");
+                            if (StringUtils.isEmpty(libraryName))
+                            {
+                                continue;
+                            }
+                            TagLibrary library = (TagLibrary) Thread
+                                    .currentThread().getContextClassLoader()
+                                    .loadClass(libraryName).newInstance();
+                            tagLibraries.put(url, library);
+                        }
                     }
-                    TagLibrary library = (TagLibrary) Thread.currentThread()
-                            .getContextClassLoader().loadClass(libraryName)
-                            .newInstance();
-                    tagLibraries.put(url, library);
+                }
+                catch (Exception e)
+                {
+                    getLogger().error(
+                            "Error occurred when loading properties "
+                                    + resource, e);
                 }
             }
+        }
+        catch (Exception e)
+        {
+            getLogger().fatal("Can't initialize tag library registry", e);
         }
     }
 
@@ -72,8 +93,9 @@ public abstract class PropertyTagLibraryRegistry
      * Specify the resource of properties file
      * 
      * @return Resource of properties file
+     * @throws Exception Throw it out
      */
-    protected abstract URL[] getRegistryResources();
+    protected abstract URL[] getRegistryResources() throws Exception;
 
     /**
      * Register all tag libraries in this registry to a given jelly context
