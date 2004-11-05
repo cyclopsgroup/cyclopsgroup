6fc634e35f9b261ab6771ac55d0f70ee60703054
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2004-11-05 06:16:22 +0000

    changed the layout

--- trunk/amulets/cyclops/src/plugin-resources/docs/language.js
+++ trunk/amulets/cyclops/src/plugin-resources/docs/language.js
@@ -6,9 +6,9 @@
  */
 function languageLink(link, lang, title, base) {
     if(lang == "en"){
-        document.write('<a href="'+link+'">');
+        document.write('<a href="'+link+'" title="' + title + '">');
     } else {
-        document.write('<a href="http://babelfish.altavista.com/babelfish/urltrurl?url='+link+'&lp=en_' + lang + '">');
+        document.write('<a href="http://babelfish.altavista.com/babelfish/urltrurl?url='+link+'&lp=en_' + lang + '" title="' + title + '">');
     }
     document.write('<img src="'+base+'/images/countryflags/'+lang+'.gif" width="27" height="18" border="0" alt="'+title+'">');
     document.write("</a>");
