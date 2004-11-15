ac18034cf2b7301735344f77ebd6d5ba6f225bad
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2004-11-15 04:56:13 +0000

    value parser added

---
+++ trunk/cyclib/src/java/main/com/cyclopsgroup/cyclib/DefaultValueParser.java
@@ -0,0 +1,78 @@
+/* ==========================================================================
+ * Copyright 2002-2004 Cyclops Group Community
+ * 
+ * Licensed under the Open Software License, Version 2.1 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://opensource.org/licenses/osl-2.1.php
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * =========================================================================
+ */
+package com.cyclopsgroup.cyclib;
+
+import java.util.Collection;
+
+import org.apache.commons.collections.MultiHashMap;
+import org.apache.commons.lang.ArrayUtils;
+
+/**
+ * Default implementation of value parser
+ * 
+ * @author <a href="mailto:jiaqi.guo@gmail.com">Jiaqi Guo </a>
+ */
+public class DefaultValueParser extends ValueParser
+{
+    private MultiHashMap values = new MultiHashMap();
+
+    /**
+     * Override or implement method of parent class or interface
+     *
+     * @see com.cyclopsgroup.cyclib.ValueParser#add(java.lang.String, java.lang.String)
+     */
+    public void add(String name, String value)
+    {
+        if (value != null)
+        {
+            values.put(name, value);
+        }
+    }
+
+    /**
+     * Override or implement method of parent class or interface
+     *
+     * @see com.cyclopsgroup.cyclib.ValueParser#doGetValue(java.lang.String)
+     */
+    protected String doGetValue(String name) throws Exception
+    {
+        String[] values = doGetValues(name);
+        return values.length == 0 ? null : values[0];
+    }
+
+    /**
+     * Override or implement method of parent class or interface
+     *
+     * @see com.cyclopsgroup.cyclib.ValueParser#doGetValues(java.lang.String)
+     */
+    protected String[] doGetValues(String name) throws Exception
+    {
+        Collection c = (Collection) values.get(name);
+        return c == null ? ArrayUtils.EMPTY_STRING_ARRAY : (String[]) c
+                .toArray(ArrayUtils.EMPTY_STRING_ARRAY);
+    }
+
+    /**
+     * Override or implement method of parent class or interface
+     *
+     * @see com.cyclopsgroup.cyclib.ValueParser#remove(java.lang.String)
+     */
+    public void remove(String name)
+    {
+        values.remove(name);
+    }
+}
--- trunk/cyclib/src/java/main/com/cyclopsgroup/cyclib/ValueParser.java
+++ trunk/cyclib/src/java/main/com/cyclopsgroup/cyclib/ValueParser.java
@@ -16,12 +16,443 @@
  */
 package com.cyclopsgroup.cyclib;
 
+import java.util.HashSet;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
+
 /**
- * TODO Add javadoc for this class
+ * Typed value parser
  * 
  * @author <a href="mailto:jiaqi.guo@gmail.com">Jiaqi Guo </a>
  */
-public class ValueParser
+public abstract class ValueParser
 {
 
+    private static HashSet booleanFalseValues = new HashSet();
+
+    private static HashSet booleanTrueValues = new HashSet();
+
+    static
+    {
+        booleanTrueValues.add("true");
+        booleanTrueValues.add("yes");
+        booleanTrueValues.add("1");
+        booleanTrueValues.add("t");
+        booleanTrueValues.add("y");
+
+        booleanFalseValues.add("false");
+        booleanFalseValues.add("no");
+        booleanFalseValues.add("0");
+        booleanFalseValues.add("f");
+        booleanFalseValues.add("n");
+    }
+
+    /** Default boolean value */
+    public boolean defaultBooleanValue = false;
+
+    /** Default double value */
+    public double defaultDoubleValue = 0.0;
+
+    /** Default float value */
+    public float defaultFloatValue = 0.0f;
+
+    /** default int value */
+    public int defaultIntValue = 0;
+
+    /** Default long value */
+    public long defaultLongValue = 0l;
+
+    /** Default string value */
+    public String defaultStringValue = StringUtils.EMPTY;
+
+    /**
+     * Add an attribute
+     *
+     * @param name Name of the attribute
+     * @param value
+     */
+    public abstract void add(String name, String value);
+
+    /**
+     * Internally get string value
+     *
+     * @param name Name of attribute
+     * @return String value of attribute
+     * @throws Exception Throw it out
+     */
+    protected abstract String doGetValue(String name) throws Exception;
+
+    /**
+     * Internally get string array value
+     *
+     * @param name Name of the attribute
+     * @return String array value of attribute
+     * @throws Exception Throw it out
+     */
+    protected abstract String[] doGetValues(String name) throws Exception;
+
+    /**
+     * Simple get method
+     *
+     * @param name Name of the attribute
+     * @return Attribute value or attribute values
+     * @throws Exception Throw it out
+     */
+    public Object get(String name) throws Exception
+    {
+        String[] values = doGetValues(name);
+        if (values == null || values.length == 0)
+        {
+            return null;
+        }
+        if (values.length == 1)
+        {
+            return values[0];
+        }
+        return values;
+    }
+
+    /**
+     * Get boolean value of attribute
+     *
+     * @param name Name of attribute
+     * @return Boolean value of attribute
+     */
+    public boolean getBoolean(String name)
+    {
+        return getBoolean(name, defaultBooleanValue);
+    }
+
+    /**
+     * Get boolean value of attribute
+     *
+     * @param name Attribute name
+     * @param defaultValue Default value
+     * @return Boolean value of attribute
+     */
+    public boolean getBoolean(String name, boolean defaultValue)
+    {
+        try
+        {
+            String ret = doGetValue(name);
+            if (StringUtils.isEmpty(ret))
+            {
+                return defaultValue;
+            }
+            if (booleanTrueValues.contains(ret))
+            {
+                return true;
+            }
+            if (booleanFalseValues.contains(ret))
+            {
+                return false;
+            }
+            return defaultValue;
+        }
+        catch (Exception e)
+        {
+            return defaultValue;
+        }
+    }
+
+    /**
+     * Get double value of attribute
+     *
+     * @param name Name of the attribute
+     * @return Double value of attribute
+     */
+    public double getDouble(String name)
+    {
+        return getDouble(name, defaultDoubleValue);
+    }
+
+    /**
+     * Get Float value of attribute
+     *
+     * @param name Name of the attribute
+     * @param defaultValue Default value of attribute
+     * @return String value of attribute
+     */
+    public double getDouble(String name, double defaultValue)
+    {
+        try
+        {
+            return Double.parseDouble(doGetValue(name));
+        }
+        catch (Exception ignored)
+        {
+            return defaultValue;
+        }
+    }
+
+    /**
+     * Get float value of attribute
+     *
+     * @param name Name of the attribute
+     * @return Float value of attribute
+     */
+    public float getFloat(String name)
+    {
+        return getFloat(name, defaultFloatValue);
+    }
+
+    /**
+     * Get Float value of attribute
+     *
+     * @param name Name of the attribute
+     * @param defaultValue Default value of attribute
+     * @return String value of attribute
+     */
+    public float getFloat(String name, float defaultValue)
+    {
+        try
+        {
+            return Float.parseFloat(doGetValue(name));
+        }
+        catch (Exception ignored)
+        {
+            return defaultValue;
+        }
+    }
+
+    /**
+     * Get int value of attribute
+     *
+     * @param name Name of the attribute
+     * @return Int value of attribute
+     */
+    public int getInt(String name)
+    {
+        return getInt(name, defaultIntValue);
+    }
+
+    /**
+     * Get int value of attribute
+     *
+     * @param name Name of the attribute
+     * @param defaultValue Default value of attribute
+     * @return Int value of attribute
+     */
+    public int getInt(String name, int defaultValue)
+    {
+        try
+        {
+            return Integer.parseInt(doGetValue(name));
+        }
+        catch (Exception ignored)
+        {
+            return defaultValue;
+        }
+    }
+
+    /**
+     * Get int value array
+     *
+     * @param name Name of the attribute
+     * @return Int value array
+     */
+    public int[] getInts(String name)
+    {
+        return getInts(name, defaultIntValue);
+    }
+
+    /**
+     * Get int values
+     *
+     * @param name Attribute name
+     * @param defaultValue Default int value
+     * @return Int array
+     */
+    public int[] getInts(String name, int defaultValue)
+    {
+        try
+        {
+            String[] values = doGetValues(name);
+            int[] ret = new int[values.length];
+            for (int i = 0; i < values.length; i++)
+            {
+                String value = values[i];
+                try
+                {
+                    ret[i] = Integer.parseInt(value);
+                }
+                catch (Exception ignored)
+                {
+                    ret[i] = defaultValue;
+                }
+            }
+            return ret;
+        }
+        catch (Exception e)
+        {
+            return ArrayUtils.EMPTY_INT_ARRAY;
+        }
+    }
+
+    /**
+     * Get long value of attribute
+     *
+     * @param name Name of the attribute
+     * @return Long value of attribute
+     */
+    public long getLong(String name)
+    {
+        return getLong(name, defaultLongValue);
+    }
+
+    /**
+     * Get long value of attribute
+     *
+     * @param name Name of the attribute
+     * @param defaultValue Default value of attribute
+     * @return Int value of attribute
+     */
+    public long getLong(String name, long defaultValue)
+    {
+        try
+        {
+            return Long.parseLong(doGetValue(name));
+        }
+        catch (Exception ignored)
+        {
+            return defaultValue;
+        }
+    }
+
+    /**
+     * Get long array value of attribute
+     *
+     * @param name Name of the attribute
+     * @return Long value
+     */
+    public long[] getLongs(String name)
+    {
+        return getLongs(name, defaultLongValue);
+    }
+
+    /**
+     * Get long array value of attribute
+     *
+     * @param name Name of the attribute
+     * @param defaultValue Default long value
+     * @return Long array
+     */
+    public long[] getLongs(String name, long defaultValue)
+    {
+        try
+        {
+            String[] values = doGetValues(name);
+            long[] ret = new long[values.length];
+            for (int i = 0; i < values.length; i++)
+            {
+                String value = values[i];
+                try
+                {
+                    ret[i] = Long.parseLong(value);
+                }
+                catch (Exception e)
+                {
+                    ret[i] = defaultValue;
+                }
+            }
+            return ret;
+        }
+        catch (Exception e)
+        {
+            return ArrayUtils.EMPTY_LONG_ARRAY;
+        }
+    }
+
+    /**
+     * Get String value of attribute
+     *
+     * @param name Name of the attribute
+     * @return String value of attribute
+     */
+    public String getString(String name)
+    {
+        return getString(name, defaultStringValue);
+    }
+
+    /**
+     * Get String value of attribute
+     *
+     * @param name Name of the attribute
+     * @param defaultValue Default value of attribute
+     * @return String value of attribute
+     */
+    public String getString(String name, String defaultValue)
+    {
+        try
+        {
+            String ret = doGetValue(name);
+            return ret == null ? defaultValue : ret;
+        }
+        catch (Exception ignored)
+        {
+            return defaultValue;
+        }
+    }
+
+    /**
+     * Get string array value of an attribute
+     *
+     * @param name Attribute name
+     * @return String array
+     */
+    public String[] getStrings(String name)
+    {
+        return getStrings(defaultStringValue);
+    }
+
+    /**
+     * Get string array
+     *
+     * @param name Name of the attribute
+     * @param defaultValue Default string value
+     * @return String array
+     */
+    public String[] getStrings(String name, String defaultValue)
+    {
+        try
+        {
+            String[] values = doGetValues(name);
+            for (int i = 0; i < values.length; i++)
+            {
+                String value = values[i];
+                if (value == null)
+                {
+                    values[i] = defaultValue;
+                }
+            }
+            return values;
+        }
+        catch (Exception e)
+        {
+            return ArrayUtils.EMPTY_STRING_ARRAY;
+        }
+    }
+
+    /**
+     * Remove an attribute
+     *
+     * @param name Name of the attribute
+     */
+    public abstract void remove(String name);
+
+    /**
+     * Set a value
+     *
+     * @param name Name of attribute
+     * @param value Value of attribute
+     */
+    public void set(String name, String value)
+    {
+        remove(name);
+        if (value != null)
+        {
+            add(name, value);
+        }
+    }
 }
