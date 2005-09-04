c8b2c3c2dea3f2ea2375e14a8ff7676128808046
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2005-09-04 18:31:00 +0000

    Several pages are added

---
+++ trunk/courselist/src/main/java/com/cyclopsgroup/courselist/ui/action/CreateCourseAction.java
@@ -0,0 +1,62 @@
+/* ==========================================================================
+ * Copyright 2002-2005 Cyclops Group Community
+ * 
+ * Licensed under the Open Software License, Version 2.1 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://opensource.org/licenses/osl-2.1.php
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * =========================================================================
+ */
+package com.cyclopsgroup.courselist.ui.action;
+
+import com.cyclopsgroup.courselist.Course;
+import com.cyclopsgroup.courselist.CoursePersistenceManager;
+import com.cyclopsgroup.waterview.Action;
+import com.cyclopsgroup.waterview.ActionContext;
+import com.cyclopsgroup.waterview.BaseServiceable;
+import com.cyclopsgroup.waterview.RuntimeData;
+
+/**
+ * @author <a href="mailto:jiaqi.guo@gmail.com">Jiaqi Guo</a>
+ * 
+ * Action to create course
+ */
+public class CreateCourseAction extends BaseServiceable implements Action
+{
+
+    /**
+     * Overwrite or implement method execute()
+     *
+     * @see com.cyclopsgroup.waterview.Action#execute(com.cyclopsgroup.waterview.RuntimeData, com.cyclopsgroup.waterview.ActionContext)
+     */
+    public void execute(RuntimeData data, ActionContext context)
+            throws Exception
+    {
+        String number = data.getParams().getString("course_number");
+        CoursePersistenceManager cpm = (CoursePersistenceManager) lookupComponent(CoursePersistenceManager.ROLE);
+        Course course = cpm.findByNumber(number);
+        if (course != null)
+        {
+            context.error("course_number", "Course " + number
+                    + " already exists");
+            return;
+        }
+
+        course = new Course();
+        course.setPrefix(data.getParams().getString("prefix"));
+        course.setTitle(data.getParams().getString("course_title"));
+        course.setNumber(number);
+        course.setDescription(data.getParams().getString("description"));
+        course.setPrerequisite(data.getParams().getString("pre_requisite"));
+        course.setCoRequisite(data.getParams().getString("co_requisite"));
+
+        cpm.save(course);
+    }
+}
---
+++ trunk/courselist/src/main/java/com/cyclopsgroup/courselist/ui/view/EditCourse.java
@@ -0,0 +1,45 @@
+/* ==========================================================================
+ * Copyright 2002-2005 Cyclops Group Community
+ * 
+ * Licensed under the Open Software License, Version 2.1 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://opensource.org/licenses/osl-2.1.php
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * =========================================================================
+ */
+package com.cyclopsgroup.courselist.ui.view;
+
+import com.cyclopsgroup.courselist.Course;
+import com.cyclopsgroup.courselist.CoursePersistenceManager;
+import com.cyclopsgroup.waterview.BaseServiceable;
+import com.cyclopsgroup.waterview.Context;
+import com.cyclopsgroup.waterview.Module;
+import com.cyclopsgroup.waterview.RuntimeData;
+
+/**
+ * @author <a href="mailto:jiaqi.guo@gmail.com">Jiaqi Guo</a>
+ * 
+ * Module to edit course
+ */
+public class EditCourse extends BaseServiceable implements Module
+{
+    /**
+     * Overwrite or implement method execute()
+     *
+     * @see com.cyclopsgroup.waterview.Module#execute(com.cyclopsgroup.waterview.RuntimeData, com.cyclopsgroup.waterview.Context)
+     */
+    public void execute(RuntimeData data, Context context) throws Exception
+    {
+        CoursePersistenceManager cpm = (CoursePersistenceManager) lookupComponent(CoursePersistenceManager.ROLE);
+        Course course = cpm.findByNumber(data.getParams().getString(
+                "course_number"));
+        context.put("course", course);
+    }
+}
