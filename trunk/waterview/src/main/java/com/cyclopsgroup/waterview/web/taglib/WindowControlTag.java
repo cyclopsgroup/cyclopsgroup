291d94e53f2e83af5c846f8c1000948504f8c1cc
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2005-09-21 20:44:02 +0000

    Window tag is enriched

--- trunk/waterview/src/main/java/com/cyclopsgroup/waterview/web/taglib/WindowControlTag.java
+++ trunk/waterview/src/main/java/com/cyclopsgroup/waterview/web/taglib/WindowControlTag.java
@@ -26,50 +26,43 @@ import com.cyclopsgroup.waterview.jelly.taglib.BaseJellyControlTag;
 public class WindowControlTag
     extends BaseJellyControlTag
 {
-    private String title;
-
-    private boolean maximizable;
+    private int border = 1;
 
     private boolean editable;
 
+    private boolean maximizable;
+
+    private int shadow = 0;
+
+    private String title;
+
     /**
-     * Getter method for editable
+     * Constructor for class WindowControlTag
      *
-     * @return Returns the editable.
      */
-    public boolean isEditable()
+    public WindowControlTag()
     {
-        return editable;
+        setScript( "/waterview/control/WindowControl.jelly" );
     }
 
     /**
-     * Setter method for editable
+     * Getter method for property border
      *
-     * @param editable The editable to set.
+     * @return Returns the border.
      */
-    public void setEditable( boolean editable )
+    public int getBorder()
     {
-        this.editable = editable;
+        return border;
     }
 
     /**
-     * Getter method for maximizable
+     * Getter method for property shadow
      *
-     * @return Returns the maximizable.
+     * @return Returns the shadow.
      */
-    public boolean isMaximizable()
+    public int getShadow()
     {
-        return maximizable;
-    }
-
-    /**
-     * Setter method for maximizable
-     *
-     * @param maximizable The maximizable to set.
-     */
-    public void setMaximizable( boolean maximizable )
-    {
-        this.maximizable = maximizable;
+        return shadow;
     }
 
     /**
@@ -83,6 +76,66 @@ public class WindowControlTag
     }
 
     /**
+     * Getter method for editable
+     *
+     * @return Returns the editable.
+     */
+    public boolean isEditable()
+    {
+        return editable;
+    }
+
+    /**
+     * Getter method for maximizable
+     *
+     * @return Returns the maximizable.
+     */
+    public boolean isMaximizable()
+    {
+        return maximizable;
+    }
+
+    /**
+     * Setter method for property border
+     *
+     * @param border The border to set.
+     */
+    public void setBorder( int border )
+    {
+        this.border = border;
+    }
+
+    /**
+     * Setter method for editable
+     *
+     * @param editable The editable to set.
+     */
+    public void setEditable( boolean editable )
+    {
+        this.editable = editable;
+    }
+
+    /**
+     * Setter method for maximizable
+     *
+     * @param maximizable The maximizable to set.
+     */
+    public void setMaximizable( boolean maximizable )
+    {
+        this.maximizable = maximizable;
+    }
+
+    /**
+     * Setter method for property shadow
+     *
+     * @param shadow The shadow to set.
+     */
+    public void setShadow( int shadow )
+    {
+        this.shadow = shadow;
+    }
+
+    /**
      * Setter method for title
      *
      * @param title The title to set.
@@ -90,14 +143,5 @@ public class WindowControlTag
     public void setTitle( String title )
     {
         this.title = title;
-    }
-
-    /**
-     * Constructor for class WindowControlTag
-     *
-     */
-    public WindowControlTag()
-    {
-        setScript( "/waterview/control/WindowControl.jelly" );
     }
 }
