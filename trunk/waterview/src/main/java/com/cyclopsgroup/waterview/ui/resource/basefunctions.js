0cba7f395b33a2c8ef1c0e802393862d2890c96e
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2005-10-10 19:10:42 +0000

    Calendar is added

--- trunk/waterview/src/main/java/com/cyclopsgroup/waterview/ui/resource/basefunctions.js
+++ trunk/waterview/src/main/java/com/cyclopsgroup/waterview/ui/resource/basefunctions.js
@@ -1,11 +1,32 @@
-function openDatePicker(formName, fieldName, baseUrl) {
-	var form = document.forms[formName];
-	var field = eval('form.' + fieldName);
-	if( field == null )
+function dateCalendarSelected(cal, date) {
+	cal.sel.value = date;
+	if (cal.dateClicked)
 	{
-		alert("No such date field [" + fieldName + "]");
-		return;
+    	cal.callCloseHandler();
+    }
+}
+
+function closeDateCalendar(cal) {
+	cal.hide();
+	_dynarch_popupCalendar = null;
+}
+
+function showDateCalendar(fieldId) {
+	var el = document.getElementById(fieldId);
+	if (_dynarch_popupCalendar != null)
+	{
+    	_dynarch_popupCalendar.hide();
+  	}
+	else
+	{
+	    var cal = new Calendar(1, null, dateCalendarSelected, closeDateCalendar);
+	    _dynarch_popupCalendar = cal;
+	    cal.setRange(1900, 2070);
+	    cal.create();
 	}
-	var url = baseUrl + "&day=" + field.value;
-	window.open(url, null, "width=300,height=200");
+	_dynarch_popupCalendar.setDateFormat("%m/%d/%Y");
+	_dynarch_popupCalendar.parseDate(el.value);
+	_dynarch_popupCalendar.sel = el;
+	_dynarch_popupCalendar.showAtElement(el.nextSibling, "span");
+	return false;
 }
