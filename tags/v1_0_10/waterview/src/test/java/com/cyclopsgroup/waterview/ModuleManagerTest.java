a50a48d8949878ac62d108a974cdc0238b2044c0
no author <noone@nowhere>
2005-10-13 17:57:04 +0000

    This commit was manufactured by cvs2svn to create tag 'v1_0_10'.

---
+++ tags/v1_0_10/waterview/src/test/java/com/cyclopsgroup/waterview/ModuleManagerTest.java
@@ -0,0 +1,27 @@
+package com.cyclopsgroup.waterview;
+
+import org.codehaus.plexus.PlexusTestCase;
+
+import com.cyclopsgroup.waterview.jelly.JellyEngine;
+import com.cyclopsgroup.waterview.spi.ModuleManager;
+
+/**
+ * @author jiaqi
+ *
+ */
+public class ModuleManagerTest
+    extends PlexusTestCase
+{
+
+    /**
+     * @throws Exception
+     */
+    public void testGetPackage()
+        throws Exception
+    {
+        lookup( JellyEngine.ROLE );
+        ModuleManager mm = (ModuleManager) lookup( ModuleManager.ROLE );
+        Path path = mm.parsePath( "/waterview/x/Xyz.jelly" );
+        System.out.println( path.getPathWithoutExtension() );
+    }
+}
