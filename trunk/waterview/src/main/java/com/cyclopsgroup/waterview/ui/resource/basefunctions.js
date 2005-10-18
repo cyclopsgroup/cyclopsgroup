7546e4ee612a455bfba511176c7ada565f6e941b
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2005-10-18 04:05:26 +0000

    AJAX javascript is added

--- trunk/waterview/src/main/java/com/cyclopsgroup/waterview/ui/resource/basefunctions.js
+++ trunk/waterview/src/main/java/com/cyclopsgroup/waterview/ui/resource/basefunctions.js
@@ -1,4 +1,7 @@
-function dateCalendarSelected(cal, date) {
+//Calendar related functions
+
+function dateCalendarSelected(cal, date)
+{
 	cal.sel.value = date;
 	if (cal.dateClicked)
 	{
@@ -6,12 +9,14 @@ function dateCalendarSelected(cal, date) {
     }
 }
 
-function closeDateCalendar(cal) {
+function closeDateCalendar(cal)
+{
 	cal.hide();
 	_dynarch_popupCalendar = null;
 }
 
-function showDateCalendar(fieldId) {
+function showDateCalendar(fieldId)
+{
 	var el = document.getElementById(fieldId);
 	if (_dynarch_popupCalendar != null)
 	{
@@ -30,3 +35,47 @@ function showDateCalendar(fieldId) {
 	_dynarch_popupCalendar.showAtElement(el.nextSibling, "span");
 	return false;
 }
+
+//AJAX related functions
+var _XMLHttpRequest;
+if(window.XMLHttpRequest)
+{
+	_XMLHttpRequest = new XMLHttpRequest();
+}
+else if(window.ActiveXObject)
+{
+	_XMLHttpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+}
+
+function fillDynaContent(tagId, url)
+{
+	if(_XMLHttpRequest == null)
+	{
+		alert("Your browser " + navigator.userAgent + " is not supported. Try other browser please.");
+		return;
+	}
+	var el = document.getElementById(tagId);
+	if(el == null)
+	{
+		return;
+	}
+    _XMLHttpRequest.open("GET", url, false);
+    _XMLHttpRequest.send("");
+    if(_XMLHttpRequest.status == "200")
+	{
+		el.innerHTML = _XMLHttpRequest.responseText;
+	}
+	else
+	{
+		alert("Something is wrong " + _XMLHttpRequest.response.Text);
+	}
+}
+
+function clearDynaContent(tagId)
+{
+	var el = document.getElementById(tagId);
+	if(el)
+	{
+		el.innerHTML = "";
+	}
+}	
