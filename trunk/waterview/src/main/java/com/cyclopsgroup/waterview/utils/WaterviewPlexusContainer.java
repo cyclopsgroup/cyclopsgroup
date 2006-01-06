701f7c99a9bcb5aae555f3675560dc3f6c09d9d0
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2006-01-06 20:49:27 +0000

    maven2.0 changes is moving to the trunk

--- trunk/waterview/src/main/java/com/cyclopsgroup/waterview/utils/WaterviewPlexusContainer.java
+++ trunk/waterview/src/main/java/com/cyclopsgroup/waterview/utils/WaterviewPlexusContainer.java
@@ -1,6 +1,6 @@
 /* ==========================================================================
  * Copyright 2002-2004 Cyclops Group Community
- * 
+ *
  * Licensed under the Open Software License, Version 2.1 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -23,18 +23,20 @@ import java.util.List;
 
 import org.codehaus.plexus.DefaultPlexusContainer;
 import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
+import org.codehaus.plexus.component.repository.io.PlexusTools;
 import org.codehaus.plexus.configuration.PlexusConfiguration;
 import org.codehaus.plexus.configuration.PlexusConfigurationException;
 import org.codehaus.plexus.configuration.PlexusConfigurationMerger;
-import org.codehaus.plexus.configuration.xml.xstream.PlexusTools;
+import org.codehaus.plexus.configuration.processor.ConfigurationProcessingException;
+import org.codehaus.plexus.configuration.processor.ConfigurationResourceNotFoundException;
 import org.codehaus.plexus.util.FileUtils;
 
 /**
  * This plexus container patchs the official plexus container.
  * It scans the directory defined by configuration-directory tag and applies LoadOnStart components
- * 
+ *
  * @task Once the bug is fixed, this class will not be necessary
- * 
+ *
  * @author <a href="mailto:jiaqi.guo@gmail.com">Jiaqi Guo </a>
  */
 public class WaterviewPlexusContainer
@@ -43,11 +45,14 @@ public class WaterviewPlexusContainer
 
     /**
      * Override or implement method of parent class or interface
+     * @throws PlexusConfigurationException Throw it out
+     * @throws ConfigurationResourceNotFoundException Throw it out
+     * @throws ConfigurationProcessingException Throw it out
      *
      * @see org.codehaus.plexus.DefaultPlexusContainer#initializeConfiguration()
      */
     protected void initializeConfiguration()
-        throws Exception
+        throws ConfigurationProcessingException, ConfigurationResourceNotFoundException, PlexusConfigurationException
     {
         super.initializeConfiguration();
         processConfigurationsDirectory();
@@ -83,13 +88,13 @@ public class WaterviewPlexusContainer
      * Process any additional component configuration files that have been
      * specified. The specified directory is scanned recursively so configurations
      * can be within nested directories to help with component organization.
-     * 
+     *
      * @task Rewrite the one in DefaultPlexusContainer since loaded configurations are not added into main configuration
-     * 
+     *
      * @throws Exception Throw it out
      */
     private void processConfigurationsDirectory()
-        throws Exception
+
     {
         String s = configuration.getChild( "configurations-directory" ).getValue( null );
 
@@ -99,16 +104,25 @@ public class WaterviewPlexusContainer
 
             if ( configurationsDirectory.exists() && configurationsDirectory.isDirectory() )
             {
-                List componentConfigurationFiles = FileUtils.getFiles( configurationsDirectory, "**/*.xml", "" );
-
-                for ( Iterator i = componentConfigurationFiles.iterator(); i.hasNext(); )
+                try
                 {
-                    File componentConfigurationFile = (File) i.next();
+                    List componentConfigurationFiles = FileUtils.getFiles( configurationsDirectory, "**/*.xml", "" );
 
-                    PlexusConfiguration componentConfiguration = PlexusTools
-                        .buildConfiguration( getInterpolationConfigurationReader( new FileReader(
-                                                                                                  componentConfigurationFile ) ) );
-                    configuration = PlexusConfigurationMerger.merge( componentConfiguration, configuration );
+                    for ( Iterator i = componentConfigurationFiles.iterator(); i.hasNext(); )
+                    {
+                        File componentConfigurationFile = (File) i.next();
+
+                        PlexusConfiguration componentConfiguration = PlexusTools
+                            .buildConfiguration(
+                                                 componentConfigurationFile.getName(),
+                                                 getInterpolationConfigurationReader( new FileReader(
+                                                                                                      componentConfigurationFile ) ) );
+                        configuration = PlexusConfigurationMerger.merge( componentConfiguration, configuration );
+                    }
+                }
+                catch ( Exception e )
+                {
+                    e.printStackTrace();
                 }
             }
         }
