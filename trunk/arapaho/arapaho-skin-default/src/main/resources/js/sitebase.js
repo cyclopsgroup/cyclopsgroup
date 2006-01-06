c9fddf67bf2e15d2d8f63230f56c6f02ae181198
crazytmac <crazytmac@gmail.com>
2006-01-06 06:34:20 +0000

    site skin is changed

--- trunk/arapaho/arapaho-skin-default/src/main/resources/js/sitebase.js
+++
@@ -1,6 +0,0 @@
-document.write('<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>');
-
-_uacct = "UA-76203-1";
-urchinTracker();
-
-document.write('<style>img {behavior: url("' + baseUrl + '/js/pngbehavior.htc");}</style>');
