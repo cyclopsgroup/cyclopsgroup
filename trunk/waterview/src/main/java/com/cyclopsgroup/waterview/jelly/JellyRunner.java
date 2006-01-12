f1d545dbc9090d287e99d8d19e6520fb3d4ccde1
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2006-01-12 05:20:12 +0000

    jetty integration

--- trunk/waterview/src/main/java/com/cyclopsgroup/waterview/jelly/JellyRunner.java
+++ trunk/waterview/src/main/java/com/cyclopsgroup/waterview/jelly/JellyRunner.java
@@ -1,6 +1,6 @@
 /* ==========================================================================
  * Copyright 2002-2005 Cyclops Group Community
- * 
+ *
  * Licensed under the COMMON DEVELOPMENT AND DISTRIBUTION LICENSE
  * (CDDL) Version 1.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,72 +28,66 @@ import java.util.Properties;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.jelly.JellyContext;
 import org.apache.commons.jelly.XMLOutput;
+import org.codehaus.plexus.DefaultPlexusContainer;
 
 import com.cyclopsgroup.waterview.Waterview;
-import com.cyclopsgroup.waterview.utils.WaterviewPlexusContainer;
 
 /**
  * @author <a href="mailto:jiaqi.guo@gmail.com">Jiaqi Guo</a>
  *
  */
-public class JellyRunner
-{
-    /**
-     * Main entry to run a script
-     * 
-     * @param args Script paths
-     * @throws Exception Throw it out
-     */
-    public static final void main( String[] args )
-        throws Exception
-    {
-        List scripts = new ArrayList();
-        for ( int i = 0; i < args.length; i++ )
-        {
-            String path = args[i];
-            File file = new File( path );
-            if ( file.isFile() )
-            {
-                scripts.add( file.toURL() );
-            }
-            else
-            {
-                Enumeration enu = JellyRunner.class.getClassLoader().getResources( path );
-                CollectionUtils.addAll( scripts, enu );
-            }
-        }
-        if ( scripts.isEmpty() )
-        {
-            System.out.println( "No script to run, return!" );
-            return;
-        }
+public class JellyRunner {
+	/**
+	 * Main entry to run a script
+	 *
+	 * @param args
+	 *            Script paths
+	 * @throws Exception
+	 *             Throw it out
+	 */
+	public static final void main(String[] args) throws Exception {
+		List scripts = new ArrayList();
+		for (int i = 0; i < args.length; i++) {
+			String path = args[i];
+			File file = new File(path);
+			if (file.isFile()) {
+				scripts.add(file.toURL());
+			} else {
+				Enumeration enu = JellyRunner.class.getClassLoader()
+						.getResources(path);
+				CollectionUtils.addAll(scripts, enu);
+			}
+		}
+		if (scripts.isEmpty()) {
+			System.out.println("No script to run, return!");
+			return;
+		}
 
-        String basedir = new File( "" ).getAbsolutePath();
-        Properties initProperties = new Properties( System.getProperties() );
-        initProperties.setProperty( "basedir", basedir );
-        initProperties.setProperty( "plexus.home", basedir );
+		String basedir = new File("").getAbsolutePath();
+		Properties initProperties = new Properties(System.getProperties());
+		initProperties.setProperty("basedir", basedir);
+		initProperties.setProperty("plexus.home", basedir);
 
-        WaterviewPlexusContainer container = new WaterviewPlexusContainer();
-        for ( Iterator j = initProperties.keySet().iterator(); j.hasNext(); )
-        {
-            String initPropertyName = (String) j.next();
-            container.addContextValue( initPropertyName, initProperties.get( initPropertyName ) );
-        }
+		DefaultPlexusContainer container = new DefaultPlexusContainer();
+		for (Iterator j = initProperties.keySet().iterator(); j.hasNext();) {
+			String initPropertyName = (String) j.next();
+			container.addContextValue(initPropertyName, initProperties
+					.get(initPropertyName));
+		}
 
-        container.addContextValue( Waterview.INIT_PROPERTIES, initProperties );
-        container.initialize();
-        container.start();
+		container.addContextValue(Waterview.INIT_PROPERTIES, initProperties);
+		container.initialize();
+		container.start();
 
-        JellyEngine je = (JellyEngine) container.lookup( JellyEngine.ROLE );
-        JellyContext jc = new JellyContext( je.getGlobalContext() );
+		JellyEngine je = (JellyEngine) container.lookup(JellyEngine.ROLE);
+		JellyContext jc = new JellyContext(je.getGlobalContext());
 
-        for ( Iterator i = scripts.iterator(); i.hasNext(); )
-        {
-            URL script = (URL) i.next();
-            System.out.print( "Running script " + script );
-            jc.runScript( script, XMLOutput.createDummyXMLOutput() );
-            System.out.println( "... Done!" );
-        }
-        container.dispose();
-    }
+		for (Iterator i = scripts.iterator(); i.hasNext();) {
+			URL script = (URL) i.next();
+			System.out.print("Running script " + script);
+			jc.runScript(script, XMLOutput.createDummyXMLOutput());
+			System.out.println("... Done!");
+		}
+		container.dispose();
+	}
 }
--- trunk/waterview/src/main/java/com/cyclopsgroup/waterview/jelly/JellyScriptsRunner.java
+++ trunk/waterview/src/main/java/com/cyclopsgroup/waterview/jelly/JellyScriptsRunner.java
@@ -29,98 +29,87 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.ExtendedProperties;
 import org.apache.commons.jelly.JellyContext;
 import org.apache.commons.jelly.XMLOutput;
+import org.codehaus.plexus.DefaultPlexusContainer;
 
 import com.cyclopsgroup.waterview.Waterview;
-import com.cyclopsgroup.waterview.utils.WaterviewPlexusContainer;
 
 /**
  * @author <a href="mailto:jiaqi.guo@gmail.com">Jiaqi Guo</a>
  *
  */
-public class JellyScriptsRunner
-{
-    /**
-     * Main entry to run a script
-     *
-     * @param args Script paths
-     * @throws Exception Throw it out
-     */
-    public static final void main( String[] args )
-        throws Exception
-    {
-        List scripts = new ArrayList();
-        for ( int i = 0; i < args.length; i++ )
-        {
-            String path = args[i];
-            File file = new File( path );
-            if ( file.isFile() )
-            {
-                scripts.add( file.toURL() );
-            }
-            else
-            {
-                Enumeration enu = JellyScriptsRunner.class.getClassLoader().getResources( path );
-                CollectionUtils.addAll( scripts, enu );
-            }
-        }
-        if ( scripts.isEmpty() )
-        {
-            System.out.println( "No script to run, return!" );
-            return;
-        }
+public class JellyScriptsRunner {
+	/**
+	 * Main entry to run a script
+	 *
+	 * @param args
+	 *            Script paths
+	 * @throws Exception
+	 *             Throw it out
+	 */
+	public static final void main(String[] args) throws Exception {
+		List scripts = new ArrayList();
+		for (int i = 0; i < args.length; i++) {
+			String path = args[i];
+			File file = new File(path);
+			if (file.isFile()) {
+				scripts.add(file.toURL());
+			} else {
+				Enumeration enu = JellyScriptsRunner.class.getClassLoader()
+						.getResources(path);
+				CollectionUtils.addAll(scripts, enu);
+			}
+		}
+		if (scripts.isEmpty()) {
+			System.out.println("No script to run, return!");
+			return;
+		}
 
-        String basedir = new File( "" ).getAbsolutePath();
-        Properties initProperties = new Properties( System.getProperties() );
-        initProperties.setProperty( "basedir", basedir );
-        initProperties.setProperty( "plexus.home", basedir );
+		String basedir = new File("").getAbsolutePath();
+		Properties initProperties = new Properties(System.getProperties());
+		initProperties.setProperty("basedir", basedir);
+		initProperties.setProperty("plexus.home", basedir);
 
-        WaterviewPlexusContainer container = new WaterviewPlexusContainer();
-        for ( Iterator j = initProperties.keySet().iterator(); j.hasNext(); )
-        {
-            String initPropertyName = (String) j.next();
-            container.addContextValue( initPropertyName, initProperties.get( initPropertyName ) );
-        }
+		DefaultPlexusContainer container = new DefaultPlexusContainer();
+		for (Iterator j = initProperties.keySet().iterator(); j.hasNext();) {
+			String initPropertyName = (String) j.next();
+			container.addContextValue(initPropertyName, initProperties
+					.get(initPropertyName));
+		}
 
-        container.addContextValue( Waterview.INIT_PROPERTIES, initProperties );
-        container.initialize();
-        container.start();
+		container.addContextValue(Waterview.INIT_PROPERTIES, initProperties);
+		container.initialize();
+		container.start();
 
-        JellyEngine je = (JellyEngine) container.lookup( JellyEngine.ROLE );
-        JellyContext jc = new JellyContext( je.getGlobalContext() );
-        XMLOutput output = XMLOutput.createXMLOutput( System.out );
-        for ( Iterator i = scripts.iterator(); i.hasNext(); )
-        {
-            URL script = (URL) i.next();
-            System.out.print( "Running script " + script );
-            ExtendedProperties ep = new ExtendedProperties();
-            ep.putAll( initProperties );
-            ep.load( script.openStream() );
-            for ( Iterator j = ep.getKeys( "script" ); j.hasNext(); )
-            {
-                String name = (String) j.next();
-                if ( name.endsWith( ".file" ) )
-                {
-                    File file = new File( ep.getString( name ) );
-                    if ( file.exists() )
-                    {
-                        System.out.println( "Runner jelly file " + file );
-                        jc.runScript( file, output );
-                    }
-                }
-                else if ( name.endsWith( ".resource" ) )
-                {
-                    Enumeration k = JellyScriptsRunner.class.getClassLoader().getResources( ep.getString( name ) );
-                    while ( j != null && k.hasMoreElements() )
-                    {
-                        URL s = (URL) k.nextElement();
-                        System.out.println( "Running jelly script " + s );
-                        jc.runScript( s, output );
-                    }
-                }
-            }
-            //jc.runScript( script, XMLOutput.createDummyXMLOutput() );
-            System.out.println( "... Done!" );
-        }
-        container.dispose();
-    }
+		JellyEngine je = (JellyEngine) container.lookup(JellyEngine.ROLE);
+		JellyContext jc = new JellyContext(je.getGlobalContext());
+		XMLOutput output = XMLOutput.createXMLOutput(System.out);
+		for (Iterator i = scripts.iterator(); i.hasNext();) {
+			URL script = (URL) i.next();
+			System.out.print("Running script " + script);
+			ExtendedProperties ep = new ExtendedProperties();
+			ep.putAll(initProperties);
+			ep.load(script.openStream());
+			for (Iterator j = ep.getKeys("script"); j.hasNext();) {
+				String name = (String) j.next();
+				if (name.endsWith(".file")) {
+					File file = new File(ep.getString(name));
+					if (file.exists()) {
+						System.out.println("Runner jelly file " + file);
+						jc.runScript(file, output);
+					}
+				} else if (name.endsWith(".resource")) {
+					Enumeration k = JellyScriptsRunner.class.getClassLoader()
+							.getResources(ep.getString(name));
+					while (j != null && k.hasMoreElements()) {
+						URL s = (URL) k.nextElement();
+						System.out.println("Running jelly script " + s);
+						jc.runScript(s, output);
+					}
+				}
+			}
+			// jc.runScript( script, XMLOutput.createDummyXMLOutput() );
+			System.out.println("... Done!");
+		}
+		container.dispose();
+	}
 }
