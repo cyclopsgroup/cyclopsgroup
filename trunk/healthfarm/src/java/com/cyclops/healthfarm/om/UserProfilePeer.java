e40e1f7278241609e1137df4548befbdde443aa6
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2006-04-01 03:33:41 +0000

    delete unused projects

--- trunk/healthfarm/src/java/com/cyclops/healthfarm/om/UserProfilePeer.java
+++
@@ -1,9 +0,0 @@
-package com.cyclops.healthfarm.om;
-
-/**
- *  You should add additional methods to this class to meet the
- *  application requirements.  This class will only be generated as
- *  long as it does not already exist in the output directory.
- */
-public class UserProfilePeer extends BaseUserProfilePeer {
-}
--- trunk/healthfarm/src/webapp/WEB-INF/temporary/readme.txt
+++
@@ -1,2 +0,0 @@
-The temporary files when uploading will be saved here.
-DO NOT delete this file!!!
