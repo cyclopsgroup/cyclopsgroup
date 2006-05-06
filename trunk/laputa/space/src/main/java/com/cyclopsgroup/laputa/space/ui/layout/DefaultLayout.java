c593d0fb56a82f8ffd206a070f2bf66fa26f2b48
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2006-05-06 06:42:52 +0000

    empty space subproject is created

---
+++ trunk/laputa/space/src/main/java/com/cyclopsgroup/laputa/space/ui/layout/DefaultLayout.java
@@ -0,0 +1,18 @@
+package com.cyclopsgroup.laputa.space.ui.layout;
+
+import com.cyclopsgroup.waterview.BaseServiceable;
+import com.cyclopsgroup.waterview.Context;
+import com.cyclopsgroup.waterview.Module;
+import com.cyclopsgroup.waterview.RunData;
+
+public class DefaultLayout extends BaseServiceable implements Module {
+
+	/**
+	 * @see com.cyclopsgroup.waterview.Module#execute(com.cyclopsgroup.waterview.RunData, com.cyclopsgroup.waterview.Context)
+	 */
+	public void execute(RunData arg0, Context arg1) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
