b6e7d2b3e7aa8845d40fbd96ead849af095c919e
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2006-05-16 23:46:19 +0000

    api changes

---
+++ trunk/waterview/impl/src/main/java/com/cyclopsgroup/waterview/ui/layout/ViewOnlyLayout.java
@@ -0,0 +1,31 @@
+package com.cyclopsgroup.waterview.ui.layout;
+
+import com.cyclopsgroup.waterview.spi.Layout;
+import com.cyclopsgroup.waterview.spi.ModuleService;
+import com.cyclopsgroup.waterview.spi.Page;
+import com.cyclopsgroup.waterview.spi.RunDataSpi;
+import com.cyclopsgroup.waterview.spi.View;
+
+/**
+ * Layout that show view only
+ * 
+ * @author jiaqi.guo@gmail.com
+ *
+ */
+public class ViewOnlyLayout
+    implements Layout
+{
+    private static final String VIEW = "/view";
+
+    /**
+     * @see com.cyclopsgroup.waterview.spi.Layout#render(com.cyclopsgroup.waterview.spi.RunDataSpi, com.cyclopsgroup.waterview.spi.Page)
+     */
+    public void render( RunDataSpi data, Page page )
+        throws Exception
+    {
+        ModuleService moduleService = (ModuleService) data.getServiceManager().lookup( ModuleService.ROLE );
+        String path = data.getPage().getPackageAlias() + VIEW + data.getPage().getPath();
+        View view = moduleService.createDynaView( path );
+        view.render( data, data.getRequestContext() );
+    }
+}
