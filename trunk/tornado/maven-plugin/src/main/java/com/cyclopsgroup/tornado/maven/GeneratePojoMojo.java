68552520e15ebde60a3202320dfe0fcce82dbde4
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2006-08-23 01:04:38 +0000

    Mojo has to follow jdk 1.4 style

--- trunk/tornado/maven-plugin/src/main/java/com/cyclopsgroup/tornado/maven/GeneratePojoMojo.java
+++ trunk/tornado/maven-plugin/src/main/java/com/cyclopsgroup/tornado/maven/GeneratePojoMojo.java
@@ -56,7 +56,6 @@ public class GeneratePojoMojo
      *
      * @see com.cyclopsgroup.tornado.maven.AbstractHibernateMojoBase#execute(org.apache.avalon.framework.service.ServiceManager)
      */
-    @Override
     public void execute( ServiceManager serviceManager )
         throws Exception
     {
