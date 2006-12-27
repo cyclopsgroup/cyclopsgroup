e1f3cea5929cbd2f7d56fd057d198a9f233beb91
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2006-12-27 22:34:08 +0000

    delete unused legacy directories

--- trunk/arapaho/arapaho-skin-default/src/main/resources/js/rsscontrol.js
+++
@@ -1,6 +0,0 @@
-function displayRss(url) {
-    var jsUrl = "http://jade.mcli.dist.maricopa.edu/feed/feed2js.php?src="
-    	+ escape(url.replace(/&amp;/g, "&"))
-    	+ "&date=y&utf=y&html=a";
-    document.writeln('<script language="javascript" src="' + jsUrl + '"></script>');
-}
