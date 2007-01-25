2c30c4010758590bd5e64a749440920c961eec94
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2007-01-25 04:28:47 +0000

    separated waterview servlet from spring waterview servlet

--- trunk/waterview/impl/src/main/java/com/cyclopsgroup/waterview/impl/servlet/AbstractSpringWaterviewServlet.java
+++ trunk/waterview/impl/src/main/java/com/cyclopsgroup/waterview/impl/servlet/AbstractSpringWaterviewServlet.java
@@ -1,41 +1,18 @@
 package com.cyclopsgroup.waterview.impl.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang.StringUtils;
 
-import com.cyclopsgroup.waterview.Context;
-import com.cyclopsgroup.waterview.RunData;
-import com.cyclopsgroup.waterview.ServiceManager;
-import com.cyclopsgroup.waterview.ServiceNotFoundException;
 import com.cyclopsgroup.waterview.impl.spring.SpringContainer;
 import com.cyclopsgroup.waterview.spi.Waterview;
 
 public abstract class AbstractSpringWaterviewServlet
-    extends HttpServlet
+    extends AbstractWaterviewServlet
 {
     private SpringContainer container;
-
-    private ServiceManager serviceManager = new ServiceManager()
-    {
-        @Override
-        public Object getService( String serviceRole )
-            throws ServiceNotFoundException
-        {
-            if ( container == null )
-            {
-                throw new IllegalStateException( "Servlet is not ready, container is still null" );
-            }
-
-            return container.getBean( serviceRole );
-        }
-    };
 
     private Waterview waterview;
 
@@ -50,54 +27,9 @@ public abstract class AbstractSpringWaterviewServlet
     }
 
     @Override
-    protected void doGet( HttpServletRequest req, HttpServletResponse resp )
-        throws ServletException, IOException
+    protected Waterview getWaterview()
     {
-        handleRequest( req, resp );
-    }
-
-    protected void doHandleException( HttpServletRequest req, HttpServletResponse resp, Exception ex )
-        throws ServletException, IOException
-    {
-        ex.printStackTrace( new PrintWriter( resp.getOutputStream() ) );
-        resp.getOutputStream().flush();
-    }
-
-    protected void doHandleRequest( HttpServletRequest req, HttpServletResponse resp )
-        throws Exception
-    {
-        RunData data = new ServletRunData( waterview, getServletContext(), req, resp );
-        Context requestContext = data.getRequestContext();
-        requestContext.put( "httpRequest", req );
-        requestContext.put( "httpResponse", resp );
-        requestContext.put( "servletContext", getServletContext() );
-        requestContext.put( "servletConfig", getServletConfig() );
-        requestContext.put( "data", data );
-        waterview.processRunData( data );
-    }
-
-    @Override
-    protected void doPost( HttpServletRequest req, HttpServletResponse resp )
-        throws ServletException, IOException
-    {
-        handleRequest( req, resp );
-    }
-
-    private void handleRequest( HttpServletRequest req, HttpServletResponse resp )
-        throws ServletException, IOException
-    {
-        try
-        {
-            doHandleRequest( req, resp );
-        }
-        catch ( Exception e )
-        {
-            doHandleException( req, resp, e );
-        }
-        finally
-        {
-            resp.getOutputStream().flush();
-        }
+        return waterview;
     }
 
     @Override
@@ -124,13 +56,6 @@ public abstract class AbstractSpringWaterviewServlet
             waterviewBeanId = Waterview.class.getName();
         }
 
-        try
-        {
-            waterview = (Waterview) serviceManager.getService( waterviewBeanId );
-        }
-        catch ( ServiceNotFoundException e )
-        {
-            throw new ServletException( "Waterview bean [" + waterviewBeanId + "] is not defined in spring container" );
-        }
+        waterview = (Waterview) container.getBean( waterviewBeanId );
     }
 }
