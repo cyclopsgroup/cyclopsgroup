482a5654c1241724e0782ea1ffd99774d0e26a20
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2007-02-03 05:24:11 +0000

    remove dependency to arapaho-lib-hibernate

--- trunk/laputa/laputa-identity-server/src/test/java/com/cyclopsgroup/laputa/identity/db/DBIdentityServiceTest.java
+++ trunk/laputa/laputa-identity-server/src/test/java/com/cyclopsgroup/laputa/identity/db/DBIdentityServiceTest.java
@@ -1,14 +1,20 @@
 package com.cyclopsgroup.laputa.identity.db;
 
-import org.hibernate.Session;
+import junit.framework.TestCase;
 
-import com.cyclopsgroup.arapaho.test.HsqlHibernateTestCase;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cache.HashtableCacheProvider;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.dialect.HSQLDialect;
+import org.hsqldb.jdbcDriver;
+
 import com.cyclopsgroup.laputa.identity.Identity;
 import com.cyclopsgroup.laputa.identity.NoSuchUserException;
 import com.cyclopsgroup.laputa.identity.IdentityService.AuthenticationResult;
 
 public class DBIdentityServiceTest
-    extends HsqlHibernateTestCase
+    extends TestCase
 {
     private DBIdentityService identityService;
 
@@ -16,7 +22,19 @@ public class DBIdentityServiceTest
     protected void setUp()
         throws Exception
     {
-        super.setUp();
+        AnnotationConfiguration cfg = new AnnotationConfiguration();
+        cfg.configure( getClass().getResource( "hibernate.xml" ) );
+        cfg.setProperty( "hibernate.dialect", HSQLDialect.class.getName() );
+        cfg.setProperty( "hibernate.connection.driver_class", jdbcDriver.class.getName() );
+        cfg.setProperty( "hibernate.connection.url", "jdbc:hsqldb:mem:test" );
+        cfg.setProperty( "hibernate.connection.username", "sa" );
+        cfg.setProperty( "hibernate.connection.pool_size", "1" );
+        cfg.setProperty( "hibernate.connection.autocommit", "true" );
+        cfg.setProperty( "hibernate.cache.provider_class", HashtableCacheProvider.class.getName() );
+        cfg.setProperty( "hibernate.hbm2ddl.auto", "create-drop" );
+        cfg.setProperty( "hibernate.show_sql", "true" );
+        SessionFactory sessionFactory = cfg.buildSessionFactory();
+
         User user = new User();
         user.setUserName( "john" );
         Session s = sessionFactory.openSession();
