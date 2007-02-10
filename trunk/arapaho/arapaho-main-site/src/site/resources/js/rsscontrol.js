187f54f388111017cd696c6555e58c61a8f5f9dc
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2007-02-10 19:57:12 +0000

    Delete legacy arapaho-main-site subproject

--- trunk/arapaho/arapaho-main-site/src/site/resources/js/rsscontrol.js
+++
@@ -1,6 +0,0 @@
-function displayRss(url) {
-    var jsUrl = "http://leeds.bates.edu/rss/rss2js.php?src="
-    	+ escape(url.replace(/&amp;/g, "&"))
-    	+ "&chan=yes&num=10&desc=yes&date=yes";
-    document.writeln('<script language="javascript" src="' + jsUrl + '"></script>');
-}
