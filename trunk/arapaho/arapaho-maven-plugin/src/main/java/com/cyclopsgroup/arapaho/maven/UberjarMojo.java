8413c2058f795119ea7fed3e9f9a3272c0fa6e50
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2007-02-21 06:21:30 +0000

    A workaround is used and uberjar mojo is usable now

--- trunk/arapaho/arapaho-maven-plugin/src/main/java/com/cyclopsgroup/arapaho/maven/UberjarMojo.java
+++ trunk/arapaho/arapaho-maven-plugin/src/main/java/com/cyclopsgroup/arapaho/maven/UberjarMojo.java
@@ -4,17 +4,20 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 import java.util.jar.JarOutputStream;
-import java.util.zip.ZipEntry;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.SystemUtils;
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugin.descriptor.PluginDescriptor;
 import org.apache.maven.project.MavenProject;
 
 /**
@@ -33,6 +36,20 @@ public class UberjarMojo
     private File artifactJarFile;
 
     /**
+     * @parameter
+     * @required
+     * @description Name of the executable main class
+     */
+    private String mainClass;
+
+    /**
+     * @parameter expression="${plugin.artifactMap}"
+     * @readonly
+     * @required
+     */
+    private Map<String, Artifact> pluginArtifactMap;
+
+    /**
      * @parameter expression="${project}"
      * @readonly
      * @required
@@ -40,24 +57,36 @@ public class UberjarMojo
     private MavenProject project;
 
     /**
-     * @parameter expression="${plugin}"
-     * @readonly
-     * @required
-     */
-    private PluginDescriptor plugin;
-
-    /**
      * @parameter expression="${basedir}/target/${project.artifactId}-uber-${project.version}.jar"
      * @required
      */
     private File uberjarFile;
 
-    /**
-     * @parameter
-     * @required
-     * @description Name of the executable main class
-     */
-    private String mainClass;
+    private void addAllEntries( JarOutputStream output, File jarFile )
+        throws IOException
+    {
+        getLog().info( "Adding everything in " + jarFile + " into uberjar..." );
+        JarFile file = new JarFile( jarFile );
+
+        for ( Enumeration<JarEntry> en = file.entries(); en.hasMoreElements(); )
+        {
+            JarEntry entry = en.nextElement();
+            if ( entry.isDirectory() )
+            {
+                continue;
+            }
+            if ( entry.getName().toLowerCase().equals( "meta-inf/manifest.mf" ) )
+            {
+                continue;
+            }
+            output.putNextEntry( new JarEntry( entry ) );
+            getLog().debug( "Adding entry " + entry.getName() + "(" + entry.getSize() + " bytes)" );
+            InputStream entryInput = file.getInputStream( entry );
+            IOUtils.copy( entryInput, output );
+            entryInput.close();
+            output.flush();
+        }
+    }
 
     private void addFileEntry( JarOutputStream output, File file, String name )
         throws IOException
@@ -68,7 +97,7 @@ public class UberjarMojo
             return;
         }
 
-        ZipEntry entry = new ZipEntry( name );
+        JarEntry entry = new JarEntry( name );
         entry.setSize( file.length() );
         entry.setTime( file.lastModified() );
         output.putNextEntry( entry );
@@ -96,6 +125,7 @@ public class UberjarMojo
 
         if ( uberjarFile.isFile() )
         {
+            getLog().info( "Deleting existing " + uberjarFile );
             uberjarFile.delete();
         }
         if ( !uberjarFile.getParentFile().isDirectory() )
@@ -107,7 +137,7 @@ public class UberjarMojo
         StringWriter classworldsConfigContent = new StringWriter();
         PrintWriter classworldsConfig = new PrintWriter( classworldsConfigContent );
         classworldsConfig.println( "main is " + mainClass + " from app" );
-        classworldsConfig.println( "[app]" + SystemUtils.LINE_SEPARATOR );
+        classworldsConfig.println( "[app]" );
 
         try
         {
@@ -119,7 +149,7 @@ public class UberjarMojo
                 if ( dependency.isFile() )
                 {
                     addFileEntry( output, dependency, "WORLDS-INF/lib/" + dependency.getName() );
-                    classworldsConfig.println( "  ${classworlds.lib}/" + dependency.getName() );
+                    classworldsConfig.println( "  load ${classworlds.lib}/" + dependency.getName() );
                 }
                 else
                 {
@@ -129,14 +159,29 @@ public class UberjarMojo
 
             //Add classworlds.conf into uberjar
             addFileEntry( output, artifactJarFile, "WORLDS-INF/lib/" + artifactJarFile.getName() );
-            classworldsConfig.println( "  ${classworlds.lib}/" + artifactJarFile.getName() );
+            classworldsConfig.println( "  load ${classworlds.lib}/" + artifactJarFile.getName() );
             getLog().info( "Adding " + "WORLDS-INF/conf/classworlds.conf" );
-            ZipEntry configEntry = new ZipEntry( "WORLDS-INF/conf/classworlds.conf" );
+            JarEntry configEntry = new JarEntry( "WORLDS-INF/conf/classworlds.conf" );
             configEntry.setSize( classworldsConfig.toString().length() );
             configEntry.setTime( System.currentTimeMillis() );
             output.putNextEntry( configEntry );
             classworldsConfig.flush();
             output.write( classworldsConfigContent.toString().getBytes() );
+
+            //Add classworlds.jar into uberjar
+            Artifact classworldsArtifact = pluginArtifactMap.get( "classworlds:classworlds" );
+            addFileEntry( output, classworldsArtifact.getFile(), "WORLDS-INF/classworlds.jar" );
+
+            //Copy everything from classworlds-boot.jar into uberjar
+            Artifact classworldsBootArtifact = pluginArtifactMap.get( "classworlds:classworlds-boot" );
+            addAllEntries( output, classworldsBootArtifact.getFile() );
+
+            //Add manifest file into uberjar
+            JarEntry manifestEntry = new JarEntry( "META-INF/MANIFEST.MF" );
+            manifestEntry.setTime( System.currentTimeMillis() );
+            output.putNextEntry( manifestEntry );
+            getLog().info( "Adding Manifest file..." );
+            IOUtils.copy( getClass().getResourceAsStream( "classworlds-manifest.txt" ), output );
 
             output.flush();
             output.close();
