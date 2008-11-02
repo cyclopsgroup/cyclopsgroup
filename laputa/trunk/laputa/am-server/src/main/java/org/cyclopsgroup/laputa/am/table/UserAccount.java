50f1c5a21e5bc642ae56e4865c0a3d5ec659d197
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2008-11-02 06:13:36 +0000

    Add placeholder classes

---
+++ laputa/trunk/laputa/am-server/src/main/java/org/cyclopsgroup/laputa/am/table/UserAccount.java
@@ -0,0 +1,19 @@
+package org.cyclopsgroup.laputa.am.table;
+
+import org.joda.time.DateTime;
+
+/**
+ * User account pojo
+ * 
+ * @author <a href="mailto:jiaqi.guo@gmail.com">Jiaqi Guo</a>
+ */
+public class UserAccount
+{
+    private String accountId;
+
+    private DateTime creationDate;
+
+    private String displayName;
+
+    private String emailAddress;
+}
---
+++ laputa/trunk/laputa/am-server/src/main/java/org/cyclopsgroup/laputa/am/table/UserSession.java
@@ -0,0 +1,25 @@
+package org.cyclopsgroup.laputa.am.table;
+
+import org.joda.time.DateTime;
+
+/**
+ * Login session
+ * 
+ * @author <a href="mailto:jiaqi.guo@gmail.com">Jiaqi Guo</a>
+ */
+public class UserSession
+{
+    private boolean acitve;
+
+    private boolean authenticated;
+
+    private String ipAddress;
+
+    private DateTime lastActiveDate;
+
+    private DateTime lastAuthenticationDate;
+
+    private String sessionId;
+
+    private DateTime startDate;
+}
