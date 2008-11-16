d0e2973b3c910aaeb7adb2ca5d8c0cb3e942788f
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2008-11-16 07:48:22 +0000

    Delete moved or deprecated directories

--- trunk/laputa/laputa-portal/src/main/java/com/cyclopsgroup/laputa/portal/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.cyclopsgroup.laputa.portal;
-
-public interface Constants
-{
-    String FORWARD_OK = "ok";
-}
--- trunk/laputa/laputa-space/src/main/java/com/cyclopsgroup/arapaho/hibernate/GenericDAO.java
+++
@@ -1,9 +0,0 @@
-package com.cyclopsgroup.arapaho.hibernate;
-
-import java.io.Serializable;
-
-public class GenericDAO
-    extends BaseHibernateGeneticDAO<Object, Serializable>
-{
-
-}
