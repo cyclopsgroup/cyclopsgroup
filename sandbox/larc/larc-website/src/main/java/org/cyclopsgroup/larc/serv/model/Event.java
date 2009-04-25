a8beda8b8dc58e9f33530befeda2867af0070991
crazytmac <crazytmac@gmail.com>
2009-04-25 06:52:18 +0000

    Continue LARC project

--- sandbox/larc/larc-website/src/main/java/org/cyclopsgroup/larc/serv/model/Event.java
+++ sandbox/larc/larc-website/src/main/java/org/cyclopsgroup/larc/serv/model/Event.java
@@ -13,15 +13,20 @@ import javax.persistence.UniqueConstraint;
 @Table( name = "larc_event", uniqueConstraints = { @UniqueConstraint( columnNames = { "secure_entity_id" } ) } )
 public class Event
 {
+    @Column( name = "description", length = 4000 )
+    private String description;
+
+    @Column( name = "event_state", nullable = false )
+    @Enumerated
+    private EventState eventState = EventState.PENDING;
+
+    @Column( name = "event_type", nullable = false )
+    @Enumerated
+    private EventType eventType = EventType.ONCE;
+
     @Column( name = "id", length = 32 )
     @Id
     private String id;
-
-    @Column( name = "title", length = 255, nullable = false )
-    private String title;
-
-    @Column( name = "description", length = 4000 )
-    private String description;
 
     @Column( name = "location", length = 255 )
     private String location;
@@ -30,11 +35,76 @@ public class Event
     @JoinColumn( name = "secure_entity_id" )
     private SecureEntity secureEntity;
 
-    @Column( name = "event_type", nullable = false )
-    @Enumerated
-    private EventType eventType = EventType.ONCE;
+    @Column( name = "title", length = 255, nullable = false )
+    private String title;
 
-    @Column( name = "event_state", nullable = false )
-    @Enumerated
-    private EventState eventState = EventState.PENDING;
+    public final String getDescription()
+    {
+        return description;
+    }
+
+    public final EventState getEventState()
+    {
+        return eventState;
+    }
+
+    public final EventType getEventType()
+    {
+        return eventType;
+    }
+
+    public final String getId()
+    {
+        return id;
+    }
+
+    public final String getLocation()
+    {
+        return location;
+    }
+
+    public final SecureEntity getSecureEntity()
+    {
+        return secureEntity;
+    }
+
+    public final String getTitle()
+    {
+        return title;
+    }
+
+    public final void setDescription( String description )
+    {
+        this.description = description;
+    }
+
+    public final void setEventState( EventState eventState )
+    {
+        this.eventState = eventState;
+    }
+
+    public final void setEventType( EventType eventType )
+    {
+        this.eventType = eventType;
+    }
+
+    public final void setId( String id )
+    {
+        this.id = id;
+    }
+
+    public final void setLocation( String location )
+    {
+        this.location = location;
+    }
+
+    public final void setSecureEntity( SecureEntity secureEntity )
+    {
+        this.secureEntity = secureEntity;
+    }
+
+    public final void setTitle( String title )
+    {
+        this.title = title;
+    }
 }
---
+++ sandbox/larc/larc-website/src/main/java/org/cyclopsgroup/larc/serv/web/AddingEvent.java
@@ -0,0 +1,9 @@
+package org.cyclopsgroup.larc.serv.web;
+
+import org.cyclopsgroup.waterview.Module;
+
+@Module(path = "/adding_event.do")
+public class AddingEvent
+{
+    
+}
