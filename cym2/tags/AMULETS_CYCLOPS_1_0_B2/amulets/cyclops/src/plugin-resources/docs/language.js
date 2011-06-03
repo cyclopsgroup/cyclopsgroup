84229882cb6c7e5c0a9bb76b616554398a725444
jiaqi <jiaqi@cyclopsgroup.org>
2011-06-02 22:33:29 -0700

    Delete entire cym2 from cyclopsgroup root repository since project is moved to cym2 repository

--- cym2/tags/AMULETS_CYCLOPS_1_0_B2/amulets/cyclops/src/plugin-resources/docs/language.js
+++
@@ -1,9 +0,0 @@
-function languageLink(link, lang, title, base) {
-    if(lang == "en"){
-        document.write('<a href="'+link+'">');
-    } else {
-        document.write('<a href="http://babelfish.altavista.com/babelfish/urltrurl?url='+link+'&lp=en_' + lang + '">');
-    }
-    document.write('<img src="'+base+'/images/countryflags/'+lang+'.gif" width="27" height="18" border="0" alt="'+title+'">');
-    document.write("</a>");
-}
