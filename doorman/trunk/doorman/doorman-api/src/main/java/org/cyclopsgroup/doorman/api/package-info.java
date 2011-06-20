3b1cd1e895b04d256b0ef40e2760930271a4d5db
jiaqi <jiaqi@cyclopsgroup.org>
2011-06-20 08:31:45 -0700

    Remove doorman from central repository

--- doorman/trunk/doorman/doorman-api/src/main/java/org/cyclopsgroup/doorman/api/package-info.java
+++
@@ -1,6 +0,0 @@
-@XmlJavaTypeAdapters( { @XmlJavaTypeAdapter( XmlDateTimeAdapter.class ) } )
-package org.cyclopsgroup.doorman.api;
-
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters;
-
--- doorman/trunk/doorman/doorman-service/src/main/java/org/cyclopsgroup/doorman/service/storage/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * JPA data POJOs
- */
-@TypeDefs( { @TypeDef( name = "datetime", typeClass = DateTimeUserType.class ) } )
-package org.cyclopsgroup.doorman.service.storage;
-
-import org.hibernate.annotations.TypeDef;
-import org.hibernate.annotations.TypeDefs;
-
