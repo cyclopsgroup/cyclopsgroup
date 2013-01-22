7206c45f350bf57199272aed35f56fa1ce36fdba
Jiaqi Guo <jguo@groupon.com>
2013-01-22 17:03:52 -0600

    Remove irrelevant SWF classes

--- minisme/src/main/java/org/cyclopsgroup/minisme/swf/message/TransitionInput.java
+++
@@ -1,9 +0,0 @@
-package org.cyclopsgroup.minisme.swf.message;
-
-import javax.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement(name="TransitionInput")
-public class TransitionInput
-{
-
-}
--- minisme/src/main/java/org/cyclopsgroup/minisme/swf/message/TransitionOutput.java
+++
@@ -1,9 +0,0 @@
-package org.cyclopsgroup.minisme.swf.message;
-
-import javax.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement( name = "TransitionOutput" )
-public class TransitionOutput
-{
-
-}
